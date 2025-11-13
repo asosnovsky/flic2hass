0c6d89e38b08d01c2c299f3043b19dad527c284b
Ari Sosnovsky <ariel@sosnovsky.ca>
2025-11-13 10:57:46 -0500

    added devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A TypeScript project with Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodejs = pkgs.nodejs_20;  # or nodejs_22, etc.
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "TypeScript dev environment loaded"
+            echo "Node.js: $(node --version)"
+          '';
+        };
+      }
+    );
+}
